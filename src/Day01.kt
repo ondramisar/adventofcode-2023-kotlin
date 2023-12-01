d1c5418a90f031c1ee4db26e3c3ebf709ce415a1
ondramisar <ondramisar124@seznam.cz>
2023-12-01 21:44:55 +0100

    day1 solution
    part2 there is a problem with regex matching. This solution is not the best but can not think of better solution right now.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val onlyDigits = line.filter { it.isDigit() }
+
+            val strNum = "${onlyDigits.first()}${onlyDigits.last()}"
+            strNum.toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+
+            val res = line.replace(Regex("(?=(one|two|three|four|five|six|seven|eight|nine))")) {
+                when (it.groups[1]?.value) {
+                    "one" -> "o1e"
+                    "two" -> "t2o"
+                    "three" -> "t3e"
+                    "four" -> "f4r"
+                    "five" -> "f5e"
+                    "six" -> "s6x"
+                    "seven" -> "s7n"
+                    "eight" -> "e8t"
+                    "nine" -> "n9e"
+                    else -> it.value
+                }
+            }
+
+            val onlyDigits = res.filter { it.isDigit() }
+
+            val strNum = "${onlyDigits.first()}${onlyDigits.last()}"
+            strNum.toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
+    //55607
     part1(input).println()
+    //55291
     part2(input).println()
 }
